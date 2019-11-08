3df011a9d28b08627a8540b4012ddf170cde1be3
Jordan Valentine <jvalen15@uncc.edu>
2019-11-07 20:59:33 -0500

    Fixed javascript issues

--- javascript.js
+++ javascript.js
@@ -1,4 +1,29 @@
 <!-- jQuery call to the accordion() method. -->
+
+// Accordion
+
 $(document).ready(function() {
     $("#tabs").accordion();
+
+    // Get today's date
+
+    var today = new Date();
+    var dd = today.getDate();
+
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+
+    if(dd<10)
+    {
+        dd='0'+dd;
+    }
+
+    if(mm<10)
+    {
+        mm='0'+mm;
+    }
+
+    today = mm +'/'+dd+'/'+yyyy;
+
+    document.getElementById("date").innerHTML = today;
 });
